07046a064499253d684c71b2c6ec8f4eae7567bf
Gowthaman M <75575846+GowthamanM@users.noreply.github.com>
2025-05-19 19:13:32 +0530

    Accounts Component Added

---
+++ src/app/home/account/account.component.ts
@@ -0,0 +1,15 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account',
+  standalone:true,
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.scss'],
+})
+export class AccountComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { WalletComponent } from './wallet/wallet.component';
+import { AccountComponent } from './account/account.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WalletComponent],
+  imports: [WalletComponent, AccountComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
